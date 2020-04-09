9b20ed6e61b8a1effe8709a9d5ee0a39a1d6609c
Andriy Pytsko <aserextensa52@gmail.com>
2020-04-09 13:24:22 +0300

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,7 +20,10 @@ class DishDetail extends Component {
                 return (
                     <div key = {item.id} className='list-unstyled m-3'>
                         <li className= "mb-1">{item.comment}</li>
-                        <li className="mb-1"> -- {item.author}, {item.date}</li>
+                        <li className="mb-1">-- {item.author}, {
+                            new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'})
+                                .format(new Date(Date.parse(item.date)))}
+                        </li>
                     </div>
 
                 );
@@ -43,12 +46,14 @@ class DishDetail extends Component {
         const dish = this.props.dish;
         if (dish != null) {
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(dish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(dish.comments)}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(dish)}
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderComments(dish.comments)}
+                        </div>
                     </div>
                 </div>
             );
